0e02f1753d7fc0d70fa5419637c52fd7d9eb0ba8
CodeWithAdityaKumar <adityakumar7079629108@gmail.com>
2024-10-17 23:40:57 +0530

    updating

--- scripts/login/adminLogin.js
+++ scripts/login/adminLogin.js
@@ -8,6 +8,15 @@ const passwordInput = document.getElementById("password");
 const errorMessage = document.getElementById("errorMessage");
 const fpassword = document.getElementById("fpassword");
 
+
+function showLoadingSpinner() {
+  document.getElementById('loadingSpinner').style.display = 'flex';
+}
+
+function hideLoadingSpinner() {
+  document.getElementById('loadingSpinner').style.display = 'none';
+}
+
 fpassword.addEventListener("click", (e)=>{
   const auth = firebase.auth();
   const email = emailInput.value;
@@ -17,11 +26,14 @@ fpassword.addEventListener("click", (e)=>{
     return;
   }
 
+  showLoadingSpinner()
   auth.sendPasswordResetEmail(email)
     .then(() => {
+      hideLoadingSpinner()
       alert("Password reset email sent. Please check your inbox.");
     })
     .catch((error) => {
+      hideLoadingSpinner()
       console.error("Password reset error:", error);
       alert("Error sending password reset email. Please try again.");
     });
@@ -32,6 +44,7 @@ fpassword.addEventListener("click", (e)=>{
 // Add login event
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  showLoadingSpinner()
 
   const email = emailInput.value;
   const password = passwordInput.value;
@@ -44,11 +57,13 @@ loginForm.addEventListener("submit", (e) => {
       const user = userCredential.user;
       console.log("User logged in:", user);
       // Redirect to admin dashboard or home page
+      hideLoadingSpinner()
       window.location.href = "/pages/login/dashboard/dashboard.html";
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
+      hideLoadingSpinner()
       console.error("Login error:", errorCode, errorMessage);
       displayError("Invalid email or password. Please try again.");
     });
